fa90bcb48d434218cebb14be5b0a188c0add363e
Pablo Grillo <pgrillo@gmail.com>
2021-04-18 17:34:27 +0200

    feat(design-tokens): add education and desing tokens (basic varibales)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { SectionWrapperCss, YearCss } from '../components/layout.styles';
 import TextCss from '../styles/text.styles';
 
 export default function Home() {
@@ -8,38 +9,63 @@ export default function Home() {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <section>
+            <SectionWrapperCss>
                 <TextCss.H3>about</TextCss.H3>
                 <p>Pablo is a user experience engineer with a demonstrated history of working in the leisure, travel & tourism industry. Leads a community of 14 Front End developers, working with an emphasis in scalability and readability.</p>
                 <p>Skilled in UI, HTML5, LESS, SASS, JavaScript, FLUX, React, Styled Components, Webpack, Jest, Enzyme, Cypress, Selenium, Google Analytics and Google Tag Manager.</p>
                 <p>He has had jobs as external contractor on worldwide consulting agencies, consolidated companies and design studies working for brands like Coke, Adidas, Carte d'or, Bacardi, etc.</p>
-            </section>
-            <section>
+            </SectionWrapperCss>
+            <SectionWrapperCss>
                 <TextCss.H3>work</TextCss.H3>
                 <dl>
-                    <dt><TextCss.H4>2016</TextCss.H4></dt>
+                    <dt><YearCss>2016</YearCss></dt>
                     <dd>
                         <TextCss.H4>ROIBACK </TextCss.H4>
                         <p>UX Engineer and Front-end, working with emphasis in scalability & readability. Conversión Rate Optimization </p>
                     </dd>
-                    <dt><TextCss.H4>2014</TextCss.H4></dt>
+                    <dt><YearCss>2014</YearCss></dt>
                     <dd>
                         <TextCss.H4>YOURTTOO.COM | OPENMARKET.TRAVEL </TextCss.H4>
                         <p>Co-Founder and CTO, providing technical vision for the Business Development of the company. Building and leading the tech team, product design and user experience.</p>
                     </dd>
-                    <dt><TextCss.H4>2013</TextCss.H4></dt>
+                    <dt><YearCss>2013</YearCss></dt>
                     <dd>
-                        <TextCss.H4>ACCENTURE INTERACTIVE | FJORD </TextCss.H4>
+                        <TextCss.H4>ACCENTURE INTERACTIVE </TextCss.H4>
                         <p>External Services Provider | Web Designer Front-End Trainer | High Traffic Ecommerce Sites
 Conversion Rate Optimization</p>
                     </dd>
-                    <dt><TextCss.H4>2009</TextCss.H4></dt>
+                    <dt><YearCss>2009</YearCss></dt>
                     <dd>
                         <TextCss.H4>ORIZONIA </TextCss.H4>
                         <p>Reporting directly to the head of Marketing and Product. UX / UI / Analytics / Project Manager</p>
                     </dd>
                 </dl>
-            </section>
+            </SectionWrapperCss>
+            <SectionWrapperCss>
+                <TextCss.H3>education</TextCss.H3>
+                <dl>
+                    <dt><YearCss>2012</YearCss></dt>
+                    <dd>
+                        <TextCss.H4>USER EXPERIENCE DESIGN <br />HUMAN COMPUTER INTERACTION </TextCss.H4>
+                        <p>Universitat Oberta de Catalunya</p>
+                    </dd>
+                    <dt><YearCss>2009</YearCss></dt>
+                    <dd>
+                        <TextCss.H4>IMAGE & SOUND DESIGNER</TextCss.H4>
+                        <p>Universidad de Buenos Aires</p>
+                    </dd>
+                    <dt><YearCss>2006</YearCss></dt>
+                    <dd>
+                        <TextCss.H4>GRAPHIC DESIGNER</TextCss.H4>
+                        <p>Universidad de Buenos Aires <em>(two years completed)</em></p>
+                    </dd>
+                    <dt><YearCss>2004</YearCss></dt>
+                    <dd>
+                        <TextCss.H4>COMPUTER SCIENCE </TextCss.H4>
+                        <p>Universidad de Buenos Aires <em>(two years completed)</em></p>
+                    </dd>
+                </dl>
+            </SectionWrapperCss>
         </Layout>
     )
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,321 @@
-const theme = {
+export const font = {
+    sourceFamilies: ['Lato:300,400,700'],
+    setBase: 'Lato',
+    sizeBase: '62.5%', // default 10px
+    styleBase: 'normal',
+
+    XXS: '1rem',
+    XS: '1.2rem',
+    S: '1.4rem',
+    B: '1.6rem',
+    L: '1.8rem',
+    XL: '2rem',
+    XXL: '2.4rem',
+    XXXL: '3rem',
+
+    bold: '700',
+    regular: '400',
+    light: '300',
+
+    lineHeightXS: '1', // default titles
+    lineHeightS: '1.3',
+    lineHeightB: '1.5', // default font body
+    lineHeightM: '1.75',
+    lineHeightL: '2',
+
+    /* default letter spacing for titles */
+    letterXS: '0',
+    letterS: '.05em',
+    letterB: '.1em',
+    letterM: '.125em',
+    letterL: '.2em',
+
+};
+// Colors
+export const colors = {
+    neutral000: 'hsla(0, 0%, 100%, 1)', // #fff
+    neutral050: 'hsla(0, 0%, 97.4%, 1)', // #f8f8f8
+    neutral100: 'hsla(0, 0%, 90%, 1)', // #e6e6e6
+    neutral200: 'hsla(0, 0%, 80%, 1)', // #cccccc
+    neutral300: 'hsla(0, 0%, 70%, 1)', // #b3b3b3
+    neutral400: 'hsla(0, 0%, 60%, 1)', // #999999
+    neutral500: 'hsla(0, 0%, 50%, 1)', // #808080
+    neutral600: 'hsla(0, 0%, 40%, 1)', // #666666
+    neutral700: 'hsla(0, 0%, 30%, 1)', // #4d4d4d
+    neutral800: 'hsla(0, 0%, 20%, 1)', // #333333
+    neutral900: 'hsla(0, 0%, 10%, 1)', // #1a1a1a
+    neutral999: 'hsla(0, 0%, 0%, 1)', // #000
+
+    transparent000: 'hsla(0, 0%, 100%, 0)',
+    transparent999: 'hsla(0, 0%, 0%, 0)',
+
+    neutralOpacity000: 'hsla(0, 0%, 100%, .5)',
+    neutralOpacity900: 'hsla(0, 0%, 10%, .5)',
+    neutralOpacity999: 'hsla(0, 0%, 0%, .5)',
+
+    primary: 'hsla(45, 37%, 54%, 1)',
+    primary025: 'hsla(45, 37%, 98%, 1)',
+    primary050: 'hsla(45, 37%, 95%, 1)',
+    primary100: 'hsla(45, 37%, 90%, 1)',
+    primary300: 'hsla(45, 37%, 70%, 1)',
+    primary500: 'hsla(45, 37%, 50%, 1)',
+    primary700: 'hsla(45, 37%, 30%, 1)',
+    primary900: 'hsla(45, 37%, 10%, 1)',
+
+    success: 'hsla(84, 50%, 45%, 1)',
+    success100: 'hsla(84, 50%, 90%, 1)',
+    success300: 'hsla(84, 50%, 70%, 1)',
+    success500: 'hsla(84, 50%, 50%, 1)',
+    success700: 'hsla(84, 50%, 30%, 1)',
+    success900: 'hsla(84, 50%, 10%, 1)',
+
+    danger: 'hsla(4, 60%, 50%, 1)',
+    danger100: 'hsla(4, 60%, 90%, 1)',
+    danger300: 'hsla(4, 60%, 70%, 1)',
+    danger500: 'hsla(4, 60%, 50%, 1)',
+    danger700: 'hsla(4, 60%, 30%, 1)',
+    danger900: 'hsla(4, 60%, 10%, 1)',
+
+    warning: 'hsla(54, 100%, 67%, 1)',
+    warning100: 'hsla(54, 100%, 90%, 1)',
+    warning300: 'hsla(54, 100%, 70%, 1)',
+    warning500: 'hsla(54, 100%, 50%, 1)',
+    warning700: 'hsla(54, 100%, 30%, 1)',
+    warning900: 'hsla(54, 100%, 10%, 1)',
+};
+
+export const layers = {
+    zBottom: '-1',
+    zBase: '0',
+    zBaseTop: '1',
+    zBaseup: '100',
+    zMediumdown: '499',
+    zMedium: '500',
+    zMediumup: '501',
+    zSuperior: '1000',
+    zTop: '1200',
+    zModal: '9999',
+};
+
+export const opacity = {
+    opacity000: '0',
+    opacity001: '.01',
+    opacity012: '.12',
+    opacity016: '.16',
+    opacity020: '.2',
+    opacity025: '.25',
+    opacity030: '.3',
+    opacity050: '.5',
+    opacity075: '.75',
+    opacity100: '1',
+};
+
+export const boxShadowRhythm = {
+    boxShadow010: '.1rem',
+    boxShadow020: '.2rem',
+    boxShadow030: '.3rem',
+    boxShadow060: '.6rem',
+    boxShadow100: '1rem',
+    boxShadow120: '1.2rem',
+    boxShadow140: '1.4rem',
+    boxShadow150: '1.5rem',
+    boxShadow190: '1.9rem',
+    boxShadow200: '2rem',
+    boxShadow280: '2.8rem',
+    boxShadow380: '3.8rem',
+    boxShadowBaseColor: 'hsl(0, 0%, 0%)',
+};
+
+export const boxShadowOpacity = {
+    boxShadowopacity000: `hsla(0, 0%, 0%, ${opacity.opacity000})`,
+    boxShadowopacity012: `hsla(0, 0%, 0%, ${opacity.opacity012})`,
+    boxShadowopacity016: `hsla(0, 0%, 0%, ${opacity.opacity016})`,
+    boxShadowopacity020: `hsla(0, 0%, 0%, ${opacity.opacity020})`,
+    boxShadowopacity025: `hsla(0, 0%, 0%, ${opacity.opacity025})`,
+    boxShadowopacity030: `hsla(0, 0%, 0%, ${opacity.opacity030})`,
+    boxShadowopacity075: `hsla(0, 0%, 0%, ${opacity.opacity075})`,
+};
+
+export const boxShadow = {
+    boxShadow: `0 ${boxShadowRhythm.boxShadow030} ${boxShadowRhythm.boxShadow030}
+    ${boxShadowOpacity.boxShadowopacity012}`,
+
+    boxShadow1: `0 0 ${boxShadowRhythm.boxShadow150} 0 ${boxShadowOpacity.boxShadowopacity020}`,
+
+    boxShadowBottom1: `0 ${boxShadowRhythm.boxShadow010} ${boxShadowRhythm.boxShadow030}
+    ${boxShadowOpacity.boxShadowopacity012}, 0 ${boxShadowRhythm.boxShadow010}
+    ${boxShadowRhythm.boxShadow020} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowBottom2: `0 ${boxShadowRhythm.boxShadow030} ${boxShadowRhythm.boxShadow060}
+    ${boxShadowOpacity.boxShadowopacity016}, 0 ${boxShadowRhythm.boxShadow030}
+    ${boxShadowRhythm.boxShadow060} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowBottom3: `0 ${boxShadowRhythm.boxShadow100} ${boxShadowRhythm.boxShadow200}
+    ${boxShadowOpacity.boxShadowopacity020}, 0 ${boxShadowRhythm.boxShadow060}
+    ${boxShadowRhythm.boxShadow060} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowBottom4: `0 ${boxShadowRhythm.boxShadow140} ${boxShadowRhythm.boxShadow280}
+    ${boxShadowOpacity.boxShadowopacity025}, 0 ${boxShadowRhythm.boxShadow100}
+    ${boxShadowRhythm.boxShadow100} ${boxShadowOpacity.boxShadowopacity020}`,
+
+    boxShadowBottom5: `0 ${boxShadowRhythm.boxShadow190} ${boxShadowRhythm.boxShadow380}
+    ${boxShadowOpacity.boxShadowopacity030}, 0 ${boxShadowRhythm.boxShadow150}
+    ${boxShadowRhythm.boxShadow120} ${boxShadowOpacity.boxShadowopacity020}`,
+
+    boxShadowTop0: `0 -${boxShadowRhythm.boxShadow030} ${boxShadowRhythm.boxShadow030}
+    ${boxShadowOpacity.boxShadowopacity012}`,
+
+    boxShadowTop1: `0 -${boxShadowRhythm.boxShadow010} ${boxShadowRhythm.boxShadow030}
+    ${boxShadowOpacity.boxShadowopacity012}, 0 -${boxShadowRhythm.boxShadow010}
+    ${boxShadowRhythm.boxShadow020} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowTop2: `0 -${boxShadowRhythm.boxShadow030} ${boxShadowRhythm.boxShadow060}
+    ${boxShadowOpacity.boxShadowopacity016}, 0 -${boxShadowRhythm.boxShadow030}
+    ${boxShadowRhythm.boxShadow060} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowTop3: `0 -${boxShadowRhythm.boxShadow100} ${boxShadowRhythm.boxShadow200}
+    ${boxShadowOpacity.boxShadowopacity020}, 0 -${boxShadowRhythm.boxShadow060}
+    ${boxShadowRhythm.boxShadow060} ${boxShadowOpacity.boxShadowopacity025}`,
+
+    boxShadowTop4: `0 -${boxShadowRhythm.boxShadow140} ${boxShadowRhythm.boxShadow280}
+    ${boxShadowOpacity.boxShadowopacity025}, 0 -${boxShadowRhythm.boxShadow100}
+    ${boxShadowRhythm.boxShadow100} ${boxShadowOpacity.boxShadowopacity020}`,
+
+    boxShadowTop5: `0 -${boxShadowRhythm.boxShadow190} ${boxShadowRhythm.boxShadow380}
+    ${boxShadowOpacity.boxShadowopacity030}, 0 -${boxShadowRhythm.boxShadow150}
+    ${boxShadowRhythm.boxShadow120} ${boxShadowOpacity.boxShadowopacity020}`,
+};
+
+export const rhythm = {
+    r010: '.1rem',
+    r025: '.25rem',
+    r050: '.5rem',
+    r075: '.75rem',
+    r100: '1rem',
+    r150: '1.5rem',
+    r200: '2rem',
+    r300: '3rem',
+    r400: '4rem',
+    r600: '6rem',
+    r800: '8rem',
+    r1000: '10rem',
+    r1200: '12rem',
+    r1600: '16rem',
+    r2000: '20rem',
+    r2500: '25rem',
+};
+
+export const animationTime = {
+    animationTimeS: '300ms',
+    animationTimeM: '500ms',
+    animationTimeL: '700ms',
+};
+
+export const borders = {
+    borderWidthS: '.1rem',
+    borderWidthM: '.2rem',
+    borderWidthL: '.3rem',
+};
+
+export const formColors = {
+    formFieldBackgorund: colors.neutral000,
+    formLabelColor: colors.neutral500,
+    formFieldColor: colors.neutral800,
+    formFieldColorFocus: colors.neutral800,
+};
+
+export const formSettings = {
+    formFieldBorderRadius: '0',
+    formFieldBorder: `${borders.borderWidthS} solid ${formColors.formFieldColor}`,
+    formFieldBorderFocus: `${borders.borderWidthM} solid ${formColors.formFieldColorFocus}`,
+    formFieldRound: rhythm.r050,
+};
+
+export const form = {
+    ...formColors,
+    ...formSettings,
+};
+
+export const icons = {
+    iconDefaultSize: '4.4rem',
+    iconsSizeXXS: '1rem',
+    iconsSizeXS: '1.4rem',
+    iconsSizeS: '1.6rem',
+    iconsSizeB: '1.8rem',
+    iconsSizeM: '2rem',
+    iconsSizeMM: '2.4rem',
+    iconsSizeML: '2.8rem',
+    iconsSizeL: '3.6rem',
+};
+
+export const images = {
+    imageCardSize: '8rem',
+    imageThumbSize: '14rem',
+    imageBackgroundSize: '24rem',
+    imageBGOverlay: `${colors.neutralOpacity999}`,
+    imageBGradientTop: `linear-gradient(to top, ${colors.neutralTransparent999}, ${colors.neutralOpacity900})`,
+    imageBGradientBottom: `linear-gradient(to bottom, ${colors.neutralTransparent999}, ${colors.neutralOpacity900})`,
+};
+
+export const base = {
+
+    round: '2.5rem',
+    roundedCard: '0',
+    /* Icon in button defaut size */
+    /* text shadow default fallback for tex over images */
+    textShadow: `0 .1rem .3rem ${boxShadowOpacity.boxShadowOpacity025}`,
+
+    sectionTitleDecorator: ' " - " ',
+    sectionTitleColor: colors.colorMain,
+
+    readMoreBGGradient: `linear-gradient(to top, ${colors.neutral000}, ${colors.neutralTransparent000})`,
+    readMoreColorText: colors.neutral500,
+    readMoreLinkColor: colors.colorMain,
+    /* DON'T DELETE  optional variables [start] */
+    /* Typos */
+    // fontSetTitle: 'Playfair Display, serif;',
+    /* Colors */
+    colorBg: colors.neutral000,
+    colorText: colors.neutral900,
+    colorLink: colors.colorCustom,
+    /* buttons */
+    // buttonPrimaryTextColor: colors.neutral000,
+    // buttonSecondaryTextColor: colors.colorMid,
+    // buttonPrimaryBgColor: colors.colorMain,
+    // buttonSecondaryBgColor: colors.neutral000,
+    // buttonPrimaryBorder: 0,
+    // buttonSecondaryBorder: `1px solid ${colors.colorAlmostLight}`
+    // buttonTextTransform: `1px solid ${colors.colorAlmostLight}`
+    // outline: `1px solid ${colors.neutral500}`,
+    // outlineDark: `1px solid ${colors.neutral200}`,
+    // outlineLight: `1px solid ${colors.neutral200}`,
+    /* DON'T DELETE  optional variables [end] */
+    outline: 'none',
+    lineHeightField: icons.iconDefaultSize,
+};
+
+const themeDefault = {
+    ...font,
+    ...colors,
+    ...layers,
+    ...opacity,
+    ...animationTime,
+    ...boxShadow,
+    ...rhythm,
+    ...borders,
+    ...form,
+    ...icons,
+    ...images,
+    ...base,
     background: {
         body: 'images/bgmin.svg',
     },
     colors: {
         default: '#FFF',
-        light: '#E5E5E5',
         primary: '#E5E5E5',
         primaryBg: '#004C4F',
+        ...colors,
     },
-}
+};
 
-export default theme;
+export default themeDefault;
