1892ea6badf16a4e240c11e4d2aaa4e779ee2346
Pablo Grillo <pgrillo@gmail.com>
2021-05-06 08:07:18 +0200

    content(prose): fix english writting

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
             </Head>
             <SectionWrapperCss>
                 <Typo.H3>about</Typo.H3>
-                <p>Pablo is a user experience engineer with a demonstrated history of working in the leisure, travel & tourism industry. Leads a community of 14 Front End developers, working with an emphasis in scalability and readability.</p>
+                <p>Pablo is a user experience engineer with a demonstrated history of working in the leisure, travel & tourism industry. He leads a community of 14 Front End developers, working with an emphasis in scalability and readability.</p>
                 <p>Skilled in UI, HTML5, LESS, SASS, JavaScript, FLUX, React, Styled Components, Webpack, Jest, Enzyme, Cypress, Selenium, Google Analytics and Google Tag Manager.</p>
-                <p>He has had jobs as external contractor on worldwide consulting agencies, consolidated companies and design studies working for brands like Coke, Adidas, Carte d'or, Bacardi, etc.</p>
+                <p>He has had jobs as external contractor in worldwide consulting agencies, consolidated companies and design studies working for brands like Coke, Adidas, Carte d'or, Bacardi, etc.</p>
             </SectionWrapperCss>
             <SectionWrapperCss>
                 <Typo.H3>work</Typo.H3>
